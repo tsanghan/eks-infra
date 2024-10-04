3b4c974557a249e5ac82255669bfbbaedbdd3ca1
Wong Tsang Han <tsanghan@gmail.com>
2024-10-04 21:02:49 +0800

    switch to gateway/certificate prodiction

--- main.tf
+++ main.tf
@@ -16,14 +16,22 @@ data "aws_route53_zone" "selected" {
   name = "sctp-sandbox.com."
 }
 
-resource "aws_route53_record" "example" {
+resource "aws_route53_record" "cymbal" {
   zone_id = data.aws_route53_zone.selected.zone_id
   name    = "cymbal"
   type    = "A"
 
   alias {
-    name                   = "a4765f5fba9ca43d4a05054b1aadb6a6-734935090.ap-southeast-1.elb.amazonaws.com"
+    name                   = "afa3ee4674e3a4ecf881480517e2a69f-47522125.ap-southeast-1.elb.amazonaws.com"
     zone_id                = "Z1LMS91P8CMLE5"
     evaluate_target_health = false
   }
+}
+
+resource "aws_route53_record" "caa" {
+  zone_id = data.aws_route53_zone.selected.zone_id
+  name    = data.aws_route53_zone.selected.name
+  type    = "CAA"
+  ttl     = 60
+  records = ["0 issue \"amazon.com\"", "128 issue \"letsencrypt.org\""]
 }
